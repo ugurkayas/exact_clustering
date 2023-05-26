537879d6ec4a8ca0358f199c0d205fbd9724b24e
ugurkayas <119409001+ugurkayas@users.noreply.github.com>
2023-05-26 15:35:00 +0300

    Update Exact_map_dp.m

--- Exact_map_dp.m
+++ Exact_map_dp.m
@@ -1,14 +1,16 @@
 clear all;
 clc;
-x=[1,1.1,1.2,1.3,1.4,5,5.1,5.2,5.3,5.4];
-N = length(x);
 
-alpha0=0.05;
+x=[5.3,1,5.1,5.4,1.2,1.4,1.3,5,5.2,1.1];                     % sample data
+x=sort(x);                                                   % sorting the data
+N= length(x);                                                % length of the data
+
+alpha0=0.05;                                                 % hyper-parameters
 sigma0=(1)*std(x);
-sigma_hat=(0.4)*std(x);
+sigma_hat=(0.5)*std(x);
 m0=mean(x);
 
-for j = 1:N
+for j = 1:N                                                  % objective value matris calculation
     for i = 1:j
         sigma_k=(1/sigma0^2+length(x(i:j))/sigma_hat^2)^(-1);
         mu_k=sigma_k*(m0/sigma0^2+sum(x(i:j))/sigma_hat^2);
@@ -20,7 +22,7 @@ for j = 1:N
     end
 end
 
-% O(N^2.L) time, O(N.L) memory implementation
+                                                            % min-plus semiring with tuples
 opplus = @opselminjoin;
 idplus.val = inf;
 idplus.list = {};
@@ -28,7 +30,7 @@ optimes = @opselpluscross;
 idtimes.val = 0;
 idtimes.list = {{}};
 
-E = cell(N+1,1);
+E = cell(N+1,1);                                            % Bellman's recursion
 for i = 1:N+1
      E{i,1} = idplus;
 end
@@ -40,21 +42,17 @@ for j = 1:N
     end
 end
 
-segs = E{N+1,1}.list;
-for k = 1:length(segs)
+clusters = E{N+1,1}.list;                                    % extracting the final clustering setting
+for k = 1:length(clusters)
     fprintf('[');
-    fprintf('(%d,%d)',segs{k}{:});
+    fprintf('(%d,%d)',clusters{k}{:});
     fprintf(']');
 end
-fprintf('\n\n');
-segs = E{N+1,1}.list{1};
-LE = length(segs);
 
-E{N+1,1}.val-gammaln(alpha0) + gammaln(alpha0+N)
+fprintf('\n\n');                                            % the final clustering setting
+E{N+1,1}.val-gammaln(alpha0) + gammaln(alpha0+N)            % final objective value
  
- 
- 
-function nl = Gaussian_nll(X,mu_k,sigma_k,sigma_hat)
+function nl = Gaussian_nll(X,mu_k,sigma_k,sigma_hat)        % Gaussian negative log-likelihood
    nl = (1/(2*(sigma_k+sigma_hat^2)))*sum((abs(X-mu_k)).^2)+(1/2)*log(sigma_k+sigma_hat^2); 
 end
 
