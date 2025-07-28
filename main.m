f19046dc8df1f656038a8885dc1b8ed5fabe8369
ugurkayas <119409001+ugurkayas@users.noreply.github.com>
2025-07-28 14:02:26 +0300

    Main.m

--- main.m
+++ main.m
@@ -17,3 +17,20 @@ m0=mean(x);
 [y2,c2]= Exact_dp_means(x,lambda);
 
 [y3,c3]= Exact_map_dp(x,alpha0,sigma0,sigma_hat,m0);
+
+% Display results
+
+fprintf('K-means:\n');
+fprintf('Error Value: %f\n', y1);
+fprintf('Cluster Setup: ');
+disp(c1);
+
+fprintf('DP-means:\n');
+fprintf('Error Value: %f\n', y2);
+fprintf('Cluster Setup: ');
+disp(c2);
+
+fprintf('MAP-DP:\n');
+fprintf('Error Value: %f\n', y3);
+fprintf('Cluster Setup: ');
+disp(c3);
